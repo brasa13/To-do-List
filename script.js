9d0dbb3ea7c19c97aaa8b04297f81a6c51985e61
Brasa <104910097+brasa13@users.noreply.github.com>
2025-09-30 17:27:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,98 @@
-const taskInput = document.getElementById("taskInput");
-const addTaskBtn = document.getElementById("addTask");
-const taskList = document.getElementById("taskList");
-const filter = document.getElementById("filter");
-
-function createTask(text) {
-  const li = document.createElement("li");
-  li.classList.add("task");
-
-  const span = document.createElement("span");
-  span.textContent = text;
-
-  const btns = document.createElement("div");
-  btns.classList.add("btns");
-
-  const doneBtn = document.createElement("button");
-  doneBtn.textContent = "✔";
-  doneBtn.classList.add("done-btn");
-  doneBtn.onclick = () => {
-    li.classList.toggle("completa");
-    applyFilter();
-  };
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "🗑";
-  deleteBtn.classList.add("delete-btn");
-  deleteBtn.onclick = () => li.remove();
-
-  btns.appendChild(doneBtn);
-  btns.appendChild(deleteBtn);
-
-  li.appendChild(span);
-  li.appendChild(btns);
-  taskList.appendChild(li);
+body {
+  font-family: Arial, sans-serif;
+  background-color: rgb(26, 24, 24);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 40vh;
+  margin: 0;
 }
 
-addTaskBtn.addEventListener("click", () => {
-  const text = taskInput.value.trim();
-  if (text) {
-    createTask(text);
-    taskInput.value = "";
+h1 {
+  color: #ffffff;
+  text-align: center;
+}
+
+.input-container {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 15px;
+  align-items: center;
+}
+
+input {
+  flex:1;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid #949494;
+  outline: none;
+}
+
+button.add-btn {
+  background: #e74c3c;
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 18px;
+}
+
+select {
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid #ddd;
+  outline: none;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.task {
+  background: #f9f9f9;
+  padding: 5px;
+  border-radius: 8px;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.task.completed span {
+  text-decoration: line-through;
+  color: gray;
+}
+
+.btns {
+  display: flex;
+  gap: 8px;
+}
+
+.done-btn {
+  background: #02a345;
+  border: none;
+  color: white;
+  padding: 6px 10px;
+  border-radius: 6px;
+  cursor: pointer;
+}
+
+.delete-btn {
+  background: #ca1602;
+  border: none;
+  color: white;
+  padding: 6px 10px;
+  border-radius: 6px;
+  cursor: pointer;
+}
+
+@media (max-width: 360px){
+  .todo-container {
+    transform: scale(0.9);  
+    transform-origin: top center;
+    margin:0 auto;
   }
-});
-
-filter.addEventListener("change", applyFilter);
-
-function applyFilter() {
-  const tasks = taskList.childNodes;
-  tasks.forEach(task => {
-    switch (filter.value) {
-      case "all":
-        task.style.display = "flex";
-        break;
-      case "completa":
-        task.style.display = task.classList.contains("completa") ? "flex" : "none";
-        break;
-      case "pending":
-        task.style.display = !task.classList.contains("completa") ? "flex" : "none";
-        break;
-    }
-  });
 }
